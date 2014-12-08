e25d78fd789a00c38fa2d40e69b367ae3e0475c3
Guityyo <guity14@gmail.com>
2014-12-08 11:32:09 +0100

    Add modification Thief, Miner and BT

--- Assets/_Scripts/Miner.cs
+++ Assets/_Scripts/Miner.cs
@@ -6,37 +6,37 @@ public enum Locations { goldmine, bar, bank, home, tree };
 
 [RequireComponent(typeof(SteerToFollow))]
 public class Miner : MonoBehaviour {
-
+	
 	private FiniteStateMachine<Miner> FSM;
-
+	
 	public Locations TargetLocation = Locations.home;
 	public int GoldCarried = 0;
 	public int MoneyInBank  = 0;
 	public int Thirst = 0;
 	public int BathroomNeed = 0;
 	public int Fatigue = 0;
-
+	
 	public SteerToFollow minerSteering;
 	public Animator animator;
-
+	
 	// Miner position
 	public Vector3 minerCurrPos;
 	public Vector3 minerTargetPos = new Vector3 ();
-
+	
 	// Target object
 	public GameObject minerTarget, mainLight;
-
+	
 	// Output channel
 	public GameObject HUD;
 	private HudController hudController;
-
+	
 	// To avoid obstacles
 	private Vector3 dir;
 	public float force = 50.0f;
 	public float minimumDistToAvoid = 2.0f;
-
+	
 	private bool gameEnded = false;
-
+	
 	public void AddToGoldCarried(int amount) {
 		GoldCarried += amount;
 	}
@@ -66,13 +66,13 @@ public class Miner : MonoBehaviour {
 	public bool IsBathroomNeedy() {
 		return BathroomNeed >= 1000; // our miner has a small bladder
 	}
-
+	
 	public bool IsAt(GameObject target) {
 		Vector3 targetPos = target.transform.position;
 		return Vector3.Distance (minerCurrPos, targetPos) <= 1.0;
 	}
-
-
+	
+	
 	public void Drinking(int amount){
 		if (GoldCarried >= Saloon.getBeerPrice()) {
 			Thirst -= amount;
@@ -85,64 +85,64 @@ public class Miner : MonoBehaviour {
 	public void IncreaseFatigue() {
 		Fatigue++;
 	}
-
+	
 	public bool IsExhausted() {
 		return Fatigue >= 2000;
 	}
-
+	
 	// Method to check if the Miner has arrived to the target
 	public bool IsNearTarget(){
 		return Vector3.Distance (minerCurrPos, minerTargetPos) <= 1.0;
 	}
-
+	
 	// Method to check if it is night
 	public bool IsDarkOutside(){
 		return mainLight.light.intensity <= 0.4;
 	}
-
-
+	
+	
 	public void ChangeState(FSMState<Miner> e) {
 		FSM.ChangeState(e);
 	}
-
+	
 	public string GetState() {
 		return FSM.ToString();
 	}
-
+	
 	public bool isChasing() {
 		return GetState() == ChaseThief.Instance.ToString();
 	}
-
+	
 	public void say (string words) {
 		hudController.setMinerText (words);
 		Debug.Log ("Miner: \"" + words + "\"");
 	}
 	
-
+	
 	public void Awake() {
 		minerTarget = GameObject.Find ("Mine");
 		mainLight = GameObject.Find ("Main light");
 		HUD = GameObject.Find ("HUD");
 		hudController = HUD.GetComponent<HudController>();
-
+		
 		say ("I'm awaking...");
-
+		
 		FSM = new FiniteStateMachine<Miner>();
 		FSM.Configure(this, GoHomeSleep.Instance);
-
+		
 		animator = GetComponent<Animator> ();
-
+		
 		//minerSteering = GetComponent<SteerToFollow> ();
-
+		
 		//set the target
 		//minerSteering.Target = minerTarget.transform;
-
+		
 		//enabling steering
 		enableSteering(5);
-
+		
 		minerCurrPos = transform.position;
 		minerTargetPos = minerSteering.Target.transform.position;
-
+		
 		dir = (minerTargetPos - minerCurrPos); // directional vector to target position
 		dir.Normalize();
 	}
@@ -155,52 +155,52 @@ public class Miner : MonoBehaviour {
 			Thirst++;
 			FSM.Update ();
 			minerCurrPos = transform.position;
-
+			
 			// AvoidObstacles method 
 			dir = (minerTargetPos - minerCurrPos);
 			dir.Normalize();
 			AvoidObstacles (ref dir);	
-
+			
 		}
 	}
-
-
+	
+	
 	public void ChangeTargetLocation(Locations l) {
 		TargetLocation = l;
-
+		
 		say ("Next stop: " + l + "!");
-
+		
 		if( minerSteering != null){
 			enableSteering(5);
 		}
 	}
-
+	
 	// To set the target of the steering behaviour
 	public void setTarget(string newTarget){
 		minerSteering = GetComponent<SteerToFollow> ();
-
+		
 		minerTarget = GameObject.Find(newTarget);
 		minerSteering.Target = minerTarget.transform;
-
+		
 		minerTargetPos = minerSteering.Target.transform.position;
 		// for obstacle avoidance
 		//dir = (minerTargetPos - minerCurrPos); // directional vector to target position
 		//dir.Normalize();
 	}
-
+	
 	// Enable steering behaviour
 	public void enableSteering(int vel){
 		minerSteering.enabled = true;
 		animator.SetInteger ("speed", vel);
 	}
-
+	
 	// Disable steering behaviour
 	public void disableSteering(){
 		minerSteering.enabled = false;
 		animator.SetInteger ("speed", 0);
 	}
 	
-
+	
 	public void quitMiningWithMessage(string message){
 		if (!gameEnded) {
 			disableSteering();
@@ -211,7 +211,7 @@ public class Miner : MonoBehaviour {
 		}
 	}
 	
-
+	
 	public void AvoidObstacles(ref Vector3 dir)
 	{
 		RaycastHit hit;
@@ -223,14 +223,14 @@ public class Miner : MonoBehaviour {
 		if (Physics.Raycast(transform.position, transform.forward, out hit, minimumDistToAvoid, layerMask))
 		{
 			say("Obstacle detected, don't wanna run into it!");
-
+			
 			//Get the normal of the hit point to calculate the new direction
 			Vector3 hitNormal = hit.normal;
 			hitNormal.y = 0.0f; //Don't want to move in Y-Space
 			
 			//Get the new directional vector by adding force to vehicle's current forward vector
 			dir = transform.forward + hitNormal * force;
-
+			
 			//Rotate the vehicle to its target directional vector
 			var rot = Quaternion.LookRotation(dir);
 			transform.rotation = Quaternion.Slerp(transform.rotation, rot, 5.0f *  Time.deltaTime);
--- Assets/_Scripts/Thief.cs
+++ Assets/_Scripts/Thief.cs
@@ -15,39 +15,39 @@ public class Thief : MonoBehaviour {
 	public int GoldCarried = 0;
 	private Miner minerScript;
 	public bool moneyStolen = false;
-
+	
 	private Vector3 dir;
 	//TODO add to avoid obstacles ??
-
+	
 	// Use this for initialization
 	void Start () {
-
+		
 		perspective = gameObject.GetComponent<Perspective>();
 		thiefSteering = GetComponent<SteerToFollow> ();
 		thiefWander = GetComponent<Wander> ();
 		animator = GetComponent<Animator> ();
 		minerScript = GameObject.Find("Miner").GetComponent<Miner>();
 		thiefCurrPos = transform.position;
-
+		
 		thiefSteering.enabled = false;
 		thiefWander.enabled = true;
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameObject mine = GameObject.Find("Mine");
 		if (perspective.IsWithinViewfield (mine))
-						Debug.Log ("jippie!");
-
+			Debug.Log ("jippie!");
+		
 		thiefCurrPos = transform.position;
 	}
-
+	
 	// Method to check if the Thief has arrived to the target
 	public bool IsNearTarget(){
 		return Vector3.Distance (thiefCurrPos, thiefTargetPos) <= 1.0;
 	}
-
+	
 	// To set the target of the steering behaviour
 	public void setTarget(string newTarget){
 		//if (!thiefTarget.Equals(newTarget)) { //TODO this doesn't work, should be fixed
@@ -57,13 +57,13 @@ public class Thief : MonoBehaviour {
 		thiefTarget = GameObject.Find(newTarget);
 		thiefSteering.Target = thiefTarget.transform;   // assign better the target
 		thiefTargetPos = thiefSteering.Target.transform.position;
-
+		
 		// for obstacle avoidance
 		dir = (thiefTargetPos - thiefCurrPos); // directional vector to target position
 		dir.Normalize();
 		//}
 	}
-
+	
 	// Enable steering behaviour and disable wandering behaviour
 	public void enableSteering(int vel){
 		thiefSteering.enabled = true;
@@ -77,28 +77,29 @@ public class Thief : MonoBehaviour {
 		thiefSteering.enabled = false;
 		animator.SetInteger ("speed", 0);
 	}
-
+	
 	public bool isAt(GameObject location){
 		return Vector3.Distance (thiefCurrPos, location.transform.position) <= 1.0;
 	}
-
+	
 	// To check if steering is enabled
 	public bool isSteeringEnabled(){
 		return thiefSteering.enabled;
 	}
-
+	
 	public void stealMoneyFromMiner() {
+		Debug.Log("THIEF: Stealing...");
 		int goldToSteal = minerScript.GoldCarried;
 		minerScript.GoldCarried -= goldToSteal;
 		GoldCarried += goldToSteal;
 		moneyStolen = true;
-		Debug.Log("Thief: I'm soo good, I just stole " + goldToSteal + " gold!");
+		Debug.Log("THIEF: I'm soo good, I just stole " + goldToSteal + " gold!");
 	}
-
+	
 	public bool hasStolenMoney(){
 		return moneyStolen;
 	}
-
+	
 	public void drinkBeer(){
 		GoldCarried -= Saloon.getBeerPrice();
 	}
