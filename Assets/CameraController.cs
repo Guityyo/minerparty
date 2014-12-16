037734d264c8d7cb2ba6dffd9f7eb7dc7cd85ecd
Nicolas Fricke <mail@nicolasfricke.de>
2014-12-16 12:55:41 +0100

    Add different cameras with toggle on 'space'

---
+++ Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject minerCameraObj;
+	public GameObject thiefCameraObj;
+	public GameObject overviewCameraObj;
+	private int activeCam;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			toggleCamera();
+		}
+	}
+
+	public void toggleCamera() {
+		if (minerCameraObj.activeSelf) {
+			minerCameraObj.SetActive(false);
+			thiefCameraObj.SetActive(true);
+		} else if (thiefCameraObj.activeSelf) {
+			thiefCameraObj.SetActive(false);
+			overviewCameraObj.SetActive(true);
+		}  else if (overviewCameraObj.activeSelf) {
+			overviewCameraObj.SetActive(false);
+			minerCameraObj.SetActive(true);
+		}
+	}
+}
